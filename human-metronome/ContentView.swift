5a7f124e4286559b05c37d6fd4a09ba478224855
Joel <58995538+jtpotato@users.noreply.github.com>
2023-09-19 18:28:41 +1000

    Initial Commit

---
+++ human-metronome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  human-metronome
+//
+//  Created by Joel Tan on 19/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ human-metronome/human_metronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  human_metronomeApp.swift
+//  human-metronome
+//
+//  Created by Joel Tan on 19/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct human_metronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
